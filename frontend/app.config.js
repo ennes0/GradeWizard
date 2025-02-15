510d62898ccb7d3ea07f2515d936300bc1a6b908
Mehmet Enes Oy <enes.2002.oy@hotmail.com>
2025-02-15 22:07:25 +0300

    Projeyi güncelliyorum

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -3,12 +3,19 @@ module.exports = {
   slug: 'gradwizard',
   version: '1.0.0',
   orientation: 'portrait',
-<<<<<<< HEAD
   android: {
     package: "com.enesoy.gradwizard",
     adaptiveIcon: {
-      foregroundImage: "./assets/images/logo0.pngs",
+      foregroundImage: "./assets/images/logo0.png",
       backgroundColor: "#FFFFFF"
+    },
+    config: {
+      googleMobileAdsAppId: "ca-app-pub-3940256099942544~3347511713"
+    }
+  },
+  ios: {
+    config: {
+      googleMobileAdsAppId: "ca-app-pub-3940256099942544~1458002511"
     }
   },
   plugins: [
@@ -21,14 +28,7 @@ module.exports = {
         delay_app_measurement_init: true,
         user_tracking_usage_description: "This identifier will be used to deliver personalized ads to you."
       }
-    ]
-  ],
-  extra: {
-    eas: {
-      projectId: "c7af8250-2584-41a8-bf2c-1df4d81266bf"
-=======
-  plugins: [
-    'expo-router',
+    ],
     [
       'expo-ads-admob',
       {
@@ -36,15 +36,9 @@ module.exports = {
       }
     ]
   ],
-  android: {
-    config: {
-      googleMobileAdsAppId: "ca-app-pub-3940256099942544~3347511713"
-    }
-  },
-  ios: {
-    config: {
-      googleMobileAdsAppId: "ca-app-pub-3940256099942544~1458002511"
->>>>>>> f6268b3023438cac41fe56306ca39537224d197a
+  extra: {
+    eas: {
+      projectId: "c7af8250-2584-41a8-bf2c-1df4d81266bf"
     }
   }
 };
