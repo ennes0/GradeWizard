91e0462e152dbfee702eaf7f015fa24d9cc532da
Mehmet Enes Oy <enes.2002.oy@hotmail.com>
2025-02-16 00:56:30 +0300

    tyu

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -11,36 +11,15 @@ module.exports = {
     adaptiveIcon: {
       foregroundImage: "./assets/images/logo0.png",
       backgroundColor: "#FFFFFF"
-    },
-    config: {
-      googleMobileAdsAppId: "ca-app-pub-3940256099942544~3347511713"
     }
   },
   ios: {
     bundleIdentifier: "com.enesoy.gradwizard",
     buildNumber: "1.0.0",
-    supportsTablet: true,
-    config: {
-      googleMobileAdsAppId: "ca-app-pub-3940256099942544~1458002511"
-    }
+    supportsTablet: true
   },
   plugins: [
-    "expo-router",
-    [
-      "react-native-google-mobile-ads",
-      {
-        android_app_id: "ca-app-pub-7510384238146981~7328107511",
-        ios_app_id: "ca-app-pub-7510384238146981~7328107511",
-        delay_app_measurement_init: true,
-        user_tracking_usage_description: "This identifier will be used to deliver personalized ads to you."
-      }
-    ],
-    [
-      'expo-ads-admob',
-      {
-        userTrackingPermission: "This identifier will be used to deliver personalized ads to you."
-      }
-    ]
+    "expo-router"
   ],
   extra: {
     eas: {
